c82bc31dea505d7517ab1a65d54b559ed56613eb
Jcmias25 <jerecuenca@hotmail.com>
2022-09-07 21:53:17 -0300

    hola

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static(path.resolve(__dirname,'public')));
+
+app.listen(2000, () => {
+
+    console.log('Servidor corriendo en el puerto 3000');
+    
+})
+
+app.get('/', (req,res) => {
+
+    res.sendFile(path.resolve(__dirname, 'src/views/home.html'))
+});
+
+
