73bb2b75eaae072ca67039b8577b3eba27494be5
Jcmias25 <jerecuenca@hotmail.com>
2022-09-20 21:38:13 -0300

    Heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.resolve(__dirname,'public')));
 
-app.listen(2000, () => {
+app.listen(process.env.PORT || 2000, () => {
 
     console.log('Servidor corriendo en el puerto 2000');
     
